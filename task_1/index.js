b6c1d5bb3d02a308f74582c1d076f4186db53f5e
Vadim <84429856+VadimN20@users.noreply.github.com>
2023-10-06 16:23:01 +0300

    Update index.js

--- task_1/index.js
+++ task_1/index.js
@@ -4,5 +4,25 @@ console.log("Let's rock")
 console.log(encoded, translations)
 
 
+function decoded(enCoded, tranSlations) {
+    const decodedNew = []
+    const uIdTemp = []
+    const len = enCoded.length;
+    for(let i = 0; i < len; i++) {
+        decodedNew[i] = enCoded[i]
+        for(let key in enCoded[i]) {
+            if (decodedNew[i][key] != null && (key != "groupId" && key != "ca" && key != "service" && key != "formatSize") && decodedNew[i][key] in tranSlations) {
+                decodedNew[i][key] = tranSlations[decodedNew[i][key]]
+            }
+            else if (!(decodedNew[i][key] in tranSlations)) {
+                //Здесь я не был уверен является ли null уникальным Id, если его нет в translations, но есть во многих значениях ключей 
+                uIdTemp.push(decodedNew[i][key])
+            }
+        }
+    }
+    const uniqueId = Array.from(new Set(uIdTemp))
+    
+    return decodedNew, uniqueId
+}
 
-// console.log(decoded)
+console.log(decoded(encoded, translations))
